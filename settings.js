56802b6a4c3ad408139044097436a3cfa7c52f58
jaxxfrost5 <5ff.jaxxlawrence@gmail.com>
2025-07-14 20:32:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,7 +13,7 @@ function convertToBool(text, trueValue = 'true') {
 
 module.exports = {
   // ===== BOT CORE SETTINGS =====
-  SESSION_ID: process.env.SESSION_ID || "", // Your bot's session ID (keep secure)
+  SESSION_ID: process.env.SESSION_ID || "malvin~J3FxFKRC#sqdMdqDkg86sNnLPtidOvGHa3ZLU0qKS0eByTPpWSMw", // Your bot's session ID (keep secure)
   PREFIX: getConfig("PREFIX") || ".", // Command prefix (e.g., ., /, !, *)
   CHATBOT: getConfig("CHATBOT") || "on", // Chatbot mode: on/off
   BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "ᴍᴀʟᴠɪɴ-xᴅ", // Bot display name
